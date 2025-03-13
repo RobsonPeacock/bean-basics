8ee6fd6c862b8d8da24164f0763fe8a3f32fe9aa
Robson Peacock <robson_peacock20@outlook.com>
2025-03-13 18:47:40 +0000

    Add home component

---
+++ src/Components/home.jsx
@@ -0,0 +1,13 @@
+import Content from '../content';
+
+const Home = () => {
+  return (
+    <>
+      <p>{Content.home.para1}</p>
+      <p>{Content.home.para2}</p>
+      <p>{Content.home.para3}</p>
+    </>
+  )
+}
+
+export default Home;
