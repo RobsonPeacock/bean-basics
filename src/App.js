a57f3785c6f6f59691140213bdd5482605f0d863
Robson Peacock <robson_peacock20@outlook.com>
2025-03-20 13:33:33 +0000

    Setup React router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/home';
 import CoffeeTypes from './Components/coffeeTypes';
 
 function App() {
   return (
-    <div className="App">
-      <CoffeeTypes />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/coffee-types" element={<CoffeeTypes />} />
+    </Routes>
   );
 }
 
