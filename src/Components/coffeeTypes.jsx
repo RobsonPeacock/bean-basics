d2cea81288bfc47c6f90b4d3ba837d74992c6aef
Robson Peacock <robson_peacock20@outlook.com>
2025-03-21 18:25:17 +0000

    Add link to coffee type details page

--- src/Components/coffeeTypes.jsx
+++ src/Components/coffeeTypes.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Content from '../content';
+import { Link } from 'react-router-dom';
 
 const coffeeTypes = () => {
   return (
     <div>
       {Object.entries(Content.coffeeTypes).map(([key, _value]) => (
         <div key={key}>
-          <p>{key}</p>
+          <Link to={key}>{key}</Link>
         </div>
       ))
 
